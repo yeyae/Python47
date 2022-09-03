8cfcdc5d8b3d65478a2fdcddb924d5496cc2540a
yeyae <whyjinny@naver.com>
2022-09-03 22:08:01 +0900

    python

---
+++ 47.py
@@ -0,0 +1,9 @@
+#프로그램의 외부에 저장된 파일을 읽는 여러가지 방법
+#readline 함수 사용하기
+#readline.py
+f = open("C:/doit/새파일.txt", 'r')
+while True:
+   line = f.readline()
+   if not line:break
+   print(line)
+   f.close()
